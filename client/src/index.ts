0b97cac7f4f0a3d53bbac3dc35d1707dcff841e0
Vitalii Runcev <vitalik.trick2001@gmai.com>
2023-03-09 20:12:19 +0200

    Fixed saving of audio files

--- client/src/index.ts
+++ client/src/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { jokeClient, voiceClient } from "./proto/config";
 import { prepareClientResponse } from "./utils";
 import * as fs from "fs";
-import * as buffer from "buffer";
+import { ServiceError } from "@grpc/grpc-js";
 
 dotenv.config({
   path: path.join(process.cwd(), "..", ".env"),
@@ -15,13 +15,13 @@ const PORT = Number(process.env.CLIENT_PORT);
 const app = express();
 
 app.get("/", (req, res) => {
-  return jokeClient.generateJoke({}, (err, value) => {
+  return jokeClient.generateJoke({}, (err: ServiceError | null, value: any) => {
     res.json(prepareClientResponse(err, value));
   });
 });
 
 app.get("/all", (req, res) => {
-  return jokeClient.findAll({}, (err, value) => {
+  return jokeClient.findAll({}, (err: ServiceError | null, value: any) => {
     res.json(prepareClientResponse(err, value));
   });
 });
@@ -31,21 +31,21 @@ app.get("/voice", (req, res) => {
     {
       joke: {
         id: 0,
-        text: "Fuck Russia, Fuck Putin, Fuck all of russians",
+        text: "Fuck Russia, Fuck Putin, Fuck all of russians pidorasssssssssss",
         tags: ["test"],
         createdAt: new Date().toISOString()
       },
     },
-    (err, value) => {
+      (err, value) => {
       if (err) throw err;
 
+        const buffer = value?.chunkData!; // Replace this with your own code to get the audio buffer
 
-      fs.writeFile("audio.mp3", value?.chunkData!, () => {
+        fs.writeFileSync(`test.wav`, buffer)
         console.log('Audio has successfully been written');
-      });
 
       res.setHeader("Content-Type", "audio/mpeg");
-      res.send(value);
+      res.send(buffer);
     }
   );
 });
